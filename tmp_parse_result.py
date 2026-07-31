b12e0b30964764a30ad6561233c22927544dbbd1
SungHoonGit <ksa134652@naver.com>
2026-07-31 09:36:04 +0900

    chore: remove temp

--- tmp_parse_result.py
+++
@@ -1,4 +0,0 @@
-import json, sys
-d = json.load(sys.stdin)
-dd = d['data']
-print('total=%d sr=%d jk=%d rem=%d t=%d' % (dd['total'], dd['siteCounts'].get('saramin',0), dd['siteCounts'].get('jobkorea',0), dd['siteCounts'].get('remember',0), dd['searchTime']))
