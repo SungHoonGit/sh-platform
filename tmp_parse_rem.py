76bfd70f1f919ad267d2e459a101e6e76627f8ca
SungHoonGit <ksa134652@naver.com>
2026-07-31 09:29:57 +0900

    chore: temp파일 제거

--- tmp_parse_rem.py
+++
@@ -1,5 +0,0 @@
-import json
-for f, n in [('/tmp/res_a.json','a'),('/tmp/res_b.json','b'),('/tmp/res_c.json','c')]:
-    d = json.load(open(f))
-    dd = d['data']
-    print('%s: total=%d rem=%d jk=%d sr=%d' % (n, dd['total'], dd['siteCounts'].get('remember',0), dd['siteCounts'].get('jobkorea',0), dd['siteCounts'].get('saramin',0)))
--- tmp_test_rem.py
+++
@@ -1,9 +0,0 @@
-import json, subprocess, time
-for i in range(3):
-    r = subprocess.check_output(['curl', '-s', '--max-time', '45', '-X', 'POST',
-        'http://localhost:8081/search',
-        '-H', 'Content-Type: application/json',
-        '-d@/tmp/rq_all_pscp.json'])
-    d = json.loads(r)['data']
-    print(str(i+1)+': total='+str(d['total'])+' rem='+str(d['siteCounts'].get('remember',0))+' jk='+str(d['siteCounts'].get('jobkorea',0))+' sr='+str(d['siteCounts'].get('saramin',0)))
-    time.sleep(2)
